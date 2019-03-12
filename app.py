56ff3e23ab7476b094610bf2ec062fd801c82cc5
ghigbie <gehigbie@gmail.com>
2019-03-11 21:14:52 -0600

    Provide if statement to check values

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ def report():
     verify_fail = 'Your useranme does not meet the requirements'
     user_pass = False
     user_name = request.args.get('username')
-    if user_name
+    if user_name[-1].isdigit and any(letter.islower() for letter in user_name) and any(letter.isupper() for letter in user_name):
+        user_pass = True
     return render_template('report.html', title=title, verify_pass=verify_pass, verify_fail=verify_fail, user_pass=user_pass, user_name=user_name)
 
 @app.errorhandler(404)
